e08a8842c6f9eb772c901b6c93d6ef1e204a9c06
FR4NKENSTEIN <jacobleesmithy@gmail.com>
2019-10-02 15:17:06 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BoxUnboxIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<object> Box = new List<object>();
+            //add some stuff to my list
+            Box.Add(7);
+            Box.Add(28);
+            Box.Add(-1);
+            Box.Add(true);
+            Box.Add("chair");
+
+            foreach (object val in Box)
+            {
+                System.Console.Write($"{val} ");
+            }
+            System.Console.WriteLine();
+
+            var sum = 0;
+            foreach (object num in Box)
+            {
+                if (num is int)
+                {
+                    int temp = (int)num;
+                    sum += temp;
+                }
+            }
+            System.Console.WriteLine(sum);
+        }
+    }
+}
